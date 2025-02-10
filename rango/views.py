e9bae7c5c52a80ab80394e71b23ce964488c33d5
Fraser Graham <2911603G@student.gla.ac.uk>
2025-02-10 13:08:25 +0000

    Chapter4Task.1

--- rango/views.py
+++ rango/views.py
@@ -12,4 +12,6 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+
+    context_dict = {'boldmessage': 'This tutorial has been put together by Fraser Graham'}
+    return render(request, 'rango/about.html', context=context_dict)
